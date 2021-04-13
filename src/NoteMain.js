eb6d186ff8d9eb93192ade1c0ecac61491b39aae
peterjuma <peterjuma@users.noreply.github.com>
2021-04-13 07:06:32 +0300

    Fix: Title render

--- src/NoteMain.js
+++ src/NoteMain.js
@@ -8,29 +8,36 @@ import TurndownService from "turndown";
 import * as turndownPluginGfm from "turndown-plugin-gfm";
 
 // Turndown
-var turndownService = new TurndownService()
-var gfm = turndownPluginGfm.gfm
-turndownService.use(gfm)
+var turndownService = new TurndownService();
+var gfm = turndownPluginGfm.gfm;
+turndownService.use(gfm);
 
 // Markdown-It & markdownitEmoji
 var md = new MarkdownIt();
 md.use(markdownitEmoji);
 
 // Task List
-md.use(markdownitTaskLists)
+md.use(markdownitTaskLists);
 
 function NoteMain(props) {
-    // var jsonData = (JSON.stringify(props.notesData, null, 2)).replace(/\\n/g, '');
-    var {notesData} = props
-    return (
-        <div className="main">
-            <div className="page-header">
-                <h2 id="notetitle-view">{notesData.notetitle}</h2>
-            </div>
-            <div className="markdown-body" id="notebody-view" dangerouslySetInnerHTML={{ __html: md.render(notesData.notebody)}} onCopy={((e) => props.handleCopyEvent(e))}>
-            </div>
-        </div>
-    )
+  // var jsonData = (JSON.stringify(props.notesData, null, 2)).replace(/\\n/g, '');
+  var { notesData } = props;
+  return (
+    <div className="main">
+      <div className="page-header">
+        <h2
+          id="notetitle-view"
+          dangerouslySetInnerHTML={{ __html: md.render(notesData.notetitle) }}
+        ></h2>
+      </div>
+      <div
+        className="markdown-body"
+        id="notebody-view"
+        dangerouslySetInnerHTML={{ __html: md.render(notesData.notebody) }}
+        onCopy={(e) => props.handleCopyEvent(e)}
+      ></div>
+    </div>
+  );
 }
 
-export default NoteMain
+export default NoteMain;
