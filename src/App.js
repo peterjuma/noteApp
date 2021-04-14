f5e27c510be31a2e06afc8017f5c8b48e6371c6a
peterjuma <peterjuma@users.noreply.github.com>
2021-04-14 16:51:19 +0300

    Fix: Empty notes error

--- src/App.js
+++ src/App.js
@@ -114,36 +114,37 @@ class App extends Component {
       var pre = codeBlock.parentNode;
       var prevElem = pre.previousElementSibling;
 
-      if (prevElem.type !== "button") {
-        var button = document.createElement("button");
-        button.className = "copy-code-button";
-        button.setAttribute("id", "copy-code-button");
-        button.type = "button";
-        button.innerText = "Copy";
+      if (prevElem && prevElem.type === "button") {
+        return;
+      }
+      var button = document.createElement("button");
+      button.className = "copy-code-button";
+      button.setAttribute("id", "copy-code-button");
+      button.type = "button";
+      button.innerText = "Copy";
 
-        button.addEventListener("click", function () {
-          clipboard.writeText(codeBlock.innerText).then(
-            function () {
-              /* Chrome doesn't seem to blur automatically,
+      button.addEventListener("click", function () {
+        clipboard.writeText(codeBlock.innerText).then(
+          function () {
+            /* Chrome doesn't seem to blur automatically,
                    leaving the button in a focused state. */
-              button.blur();
-              button.innerText = "Copied!";
-              setTimeout(function () {
-                button.innerText = "Copy";
-              }, 2000);
-            },
-            function (error) {
-              button.innerText = "Error";
-            }
-          );
-        });
+            button.blur();
+            button.innerText = "Copied!";
+            setTimeout(function () {
+              button.innerText = "Copy";
+            }, 2000);
+          },
+          function (error) {
+            button.innerText = "Error";
+          }
+        );
+      });
 
-        if (pre.parentNode.classList.contains("highlight")) {
-          var highlight = pre.parentNode;
-          highlight.parentNode.insertBefore(button, highlight);
-        } else {
-          pre.parentNode.insertBefore(button, pre);
-        }
+      if (pre.parentNode.classList.contains("highlight")) {
+        var highlight = pre.parentNode;
+        highlight.parentNode.insertBefore(button, highlight);
+      } else {
+        pre.parentNode.insertBefore(button, pre);
       }
     });
   };
